983e96ad799f9a6fdef466e21f5910be5226b059
BT_1N3_19@bktech.local <BT_1N3_19@bktech.local>
2018-10-25 11:14:21 -0400

    Initial commit

---
+++ src/com/company/HorseRunner.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class HorseRunner {
+
+    public static void main(String[] args) {
+	// write your code here
+        Horse horse1 = new Mustang ("Secretariat", 12000);
+        Horse horse2 = new Mustang("Dusty Trail", 12000);
+        Horse horse3 = new Mustang ("Silver", 12000);
+
+    }
+}
